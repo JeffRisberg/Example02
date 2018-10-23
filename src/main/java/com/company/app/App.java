f4dfdc0a5f56b058751d27b26586d941ccaed2b9
JeffRisberg <jrisberg@aol.com>
2018-10-22 23:18:58 -0700

    added editorconfig

--- src/main/java/com/company/app/App.java
+++ src/main/java/com/company/app/App.java
@@ -5,16 +5,16 @@ package com.company.app;
  */
 public class App {
 
-    private final String message = "Hello World!";
+  private final String message = "Hello World!";
 
-    public App() {
-    }
+  public App() {
+  }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getMessage());
-    }
+  public static void main(String[] args) {
+    System.out.println(new App().getMessage());
+  }
 
-    private final String getMessage() {
-        return message;
-    }
+  private final String getMessage() {
+    return message;
+  }
 }
