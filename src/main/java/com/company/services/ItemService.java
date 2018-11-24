40f0f5b7ded15afb376c8a43696b9dff39369582
JeffRisberg <jrisberg@aol.com>
2018-11-23 23:56:06 -0800

    added itemservice and its test

---
+++ src/main/java/com/company/services/ItemService.java
@@ -0,0 +1,31 @@
+package com.company.services;
+
+import com.company.models.Item;
+import com.company.stores.ItemStore;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+
+public class ItemService {
+
+  private ItemStore userStore = new ItemStore();
+
+  public int getAveragePrice() {
+
+    List<Item> items = userStore.readAllItems();
+
+    int sumOfPrices = 0;
+    for (Item item : items) {
+      sumOfPrices += item.getPrice();
+    }
+
+    return (sumOfPrices / items.size());
+  }
+
+  public String getItemNameUpperCase(long itemId) {
+
+    Item item = userStore.findById(itemId);
+
+    return StringUtils.upperCase(item.getName());
+  }
+}
---
+++ src/test/java/com/company/ItemServiceTest.java
@@ -0,0 +1,75 @@
+package com.company;
+
+import com.company.models.Item;
+import com.company.services.ItemService;
+import com.company.stores.ItemStore;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
+
+public class ItemServiceTest {
+
+  @Mock
+  private ItemStore itemStore;
+
+  @InjectMocks
+  private ItemService itemService;
+
+  @Before
+  public void setUp() throws Exception {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  public void getItemNameUpperCase() {
+
+    //
+    // Given
+    //
+    Item mockedItem = new Item(1L, "Item 1", "This is item 1", 2000);
+    when(itemStore.findById(1L)).thenReturn(mockedItem);
+
+    //
+    // When
+    //
+    String result = itemService.getItemNameUpperCase(1L);
+
+    //
+    // Then
+    //
+    verify(itemStore, times(1)).findById(22L);
+    assertThat(result, is("ITEM 1"));
+  }
+
+  @Test
+  public void getAveragePrice() {
+    //
+    // Given
+    //
+    Item mockedItem1 = new Item(11L, "Item 1", "This is item 1", 2000);
+    Item mockedItem2 = new Item(22L, "Item 2", "This is item 2", 4000);
+    List<Item> mockedItems = new ArrayList<>();
+    mockedItems.add(mockedItem1);
+    mockedItems.add(mockedItem2);
+
+    when(itemStore.readAllItems()).thenReturn(mockedItems);
+
+    //
+    // When
+    //
+    int result = itemService.getAveragePrice();
+
+    verify(itemStore, times(1)).readAllItems();
+    assertEquals(3000, result);
+  }
+}
