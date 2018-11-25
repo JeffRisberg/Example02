b9337b119ff7ffc495d5c85f0d1fc423c3aee21b
JeffRisberg <jrisberg@aol.com>
2018-11-24 23:58:47 -0800

    added more tests

--- src/test/java/com/company/ItemServiceTest.java
+++ src/test/java/com/company/ItemServiceTest.java
@@ -14,7 +14,6 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-@RunWith(MockitoJUnitRunner.class)
 public class ItemServiceTest {
 
   private ItemService itemService;
---
+++ src/test/java/com/company/ItemServiceTestWithAnnotations.java
@@ -0,0 +1,81 @@
+package com.company;
+
+import com.company.models.Item;
+import com.company.services.ItemService;
+import com.company.stores.ItemStore;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ItemServiceTestWithAnnotations {
+
+  @Mock
+  private ItemStore itemStore;
+
+  @InjectMocks
+  private ItemService itemService;
+
+  @Before
+  public void setUp() throws Exception {
+    Item mockedItem1 = new Item(1L, "Item 1", "This is item 1", 2000);
+    Item mockedItem2 = new Item(2L, "Item 2", "This is item 2", 4000);
+    List<Item> mockedItems = new ArrayList<>();
+    mockedItems.add(mockedItem1);
+    mockedItems.add(mockedItem2);
+
+    when(itemStore.readAllItems()).thenReturn(mockedItems);
+
+    when(itemStore.findById(1L)).thenReturn(mockedItem1);
+    when(itemStore.findById(2L)).thenReturn(mockedItem2);
+  }
+
+  @Before
+  public void setUp() throws Exception {
+    Injector injector = Guice.createInjector(new MockAppModule());
+
+    itemService = injector.getInstance(ItemService.class);
+    itemStore = itemService.getItemStore();
+  }
+
+  @Test
+  public void getItemNameUpperCase() {
+    //
+    // When
+    //
+    String result = itemService.getItemNameUpperCase(1L);
+
+    //
+    // Verify
+    //
+    verify(itemStore, times(1)).findById(1L);
+    assertThat(result, is("ITEM 1"));
+  }
+
+  @Test
+  public void getAveragePrice() {
+    //
+    // When
+    //
+    int result = itemService.getAveragePrice();
+
+    //
+    // Verify
+    //
+    verify(itemStore, times(1)).readAllItems();
+    assertEquals(3000, result);
+  }
+}
